3c6f3ba4e3f8a8e18429b637c2babd66da28b94f
William <william.l.danielsson@gmail.com>
2014-03-25 11:34:00 +0100

    Tog bort ännu mer onödiga filer

--- src/gov/polisen/ainappen/LoginAcitivity.java
+++ src/gov/polisen/ainappen/LoginAcitivity.java
@@ -1,16 +1,12 @@
 package gov.polisen.ainappen;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 public class LoginAcitivity extends Activity {
 
@@ -30,25 +26,6 @@ public class LoginAcitivity extends Activity {
 		startActivity(intent);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login_acitivity, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 
 	/**
 	 * A placeholder fragment containing a simple view.
