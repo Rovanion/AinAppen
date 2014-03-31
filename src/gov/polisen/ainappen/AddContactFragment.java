58f51541e96170de2f05ed1f0c11e94cdf9c2c93
Jakob Baecklund <jakob.baecklund@gmail.com>
2014-03-31 15:14:21 +0200

    Tog bort en knapp som inte användes.

--- src/gov/polisen/ainappen/AddContactFragment.java
+++ src/gov/polisen/ainappen/AddContactFragment.java
@@ -6,12 +6,10 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 public class AddContactFragment extends Fragment {
 
-	private Button submitContactButton;
-	
+
 	public AddContactFragment() {
 		// Empty constructor required for fragment subclasses
 	}
