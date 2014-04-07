83509fabe21da5b59fe83ccad8cb5f3f38a4d92a
William <william.l.danielsson@gmail.com>
2014-04-07 15:13:06 +0200

    La till Overlay för att märka ut förhinder!

--- src/gov/polisen/ainappen/MapFragment.java
+++ src/gov/polisen/ainappen/MapFragment.java
@@ -2,17 +2,15 @@ package gov.polisen.ainappen;
 
 import java.util.ArrayList;
 
-import org.osmdroid.DefaultResourceProxyImpl;
-import org.osmdroid.ResourceProxy;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.MyLocationOverlay;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -22,15 +20,17 @@ import android.view.MenuItem;
 
 public class MapFragment extends Activity implements LocationListener {
 
-	String									PROVIDER					= LocationManager.GPS_PROVIDER;
+	String									PROVIDER						= LocationManager.GPS_PROVIDER;
 
 	MapView									mapView;
 	MapController						mapController;
 	LocationManager					locationManager;
 	ArrayList<OverlayItem>	overlayItemArray;
 	double									myLatitude, myLongitude;
+	MenuItem								addObstacleMenuItem;
+	static Boolean					addObstacleEnabled	= false;
 
-	MyLocationOverlay				myLocationOverlay	= null;
+	MyLocationNewOverlay		myLocationOverlay		= null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -40,41 +40,52 @@ public class MapFragment extends Activity implements LocationListener {
 		mapView = (MapView) this.findViewById(R.id.mapView);
 		mapView.setBuiltInZoomControls(true);
 		mapView.setMultiTouchControls(true);
+		mapView.setClickable(true);
 		mapController = (MapController) mapView.getController();
 		mapController.setZoom(14);
 
-		Drawable marker = getResources()
-				.getDrawable(android.R.drawable.star_big_on);
-		int markerWidth = marker.getIntrinsicWidth();
-		int markerHeight = marker.getIntrinsicHeight();
-		marker.setBounds(0, markerHeight, markerWidth, 0);
-
-		ResourceProxy resourceProxy = new DefaultResourceProxyImpl(
-				getApplicationContext());
-
-		myLocationOverlay = new MyLocationOverlay(this, mapView);
+		myLocationOverlay = new MyLocationNewOverlay(this, mapView);
 		mapView.getOverlays().add(myLocationOverlay);
 		myLocationOverlay.enableMyLocation();
 
 		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
 		Location location = locationManager.getLastKnownLocation(PROVIDER);
-		GeoPoint currentLocation = new GeoPoint(location.getLatitude(),
-				location.getLongitude());
+		if (location != null) {
+			GeoPoint currentLocation = new GeoPoint(location.getLatitude(),
+					location.getLongitude());
 
-		mapController.setCenter(currentLocation);
+			mapController.setCenter(currentLocation);
+		} else {
+			GeoPoint startPoint = new GeoPoint(58.4109, 15.6216);
+			mapController.setCenter(startPoint);
+		}
 
-		/*
-		 * // The map starting location is Linköping, this will be my location
-		 * GeoPoint startPoint = new GeoPoint(58.4109, 15.6216);
-		 * mapController.setCenter(startPoint);
-		 */
+		// Create an ArrayList with overlays to display objects on map
+		overlayItemArray = new ArrayList<OverlayItem>();
 
+		// Create som init objects
+		OverlayItem linkopingItem = new OverlayItem("Linkoping", "Hej",
+				new GeoPoint(58.4109, 15.6216));
+		OverlayItem stockholmItem = new OverlayItem("Stockholm", "Bajs",
+				new GeoPoint(59.3073348, 18.0747967));
+
+		// Add the init objects to the ArrayList overlayItemArray
+		overlayItemArray.add(linkopingItem);
+		overlayItemArray.add(stockholmItem);
+
+		// Add the Array to the IconOverlay
+		ItemizedIconOverlay<OverlayItem> itemizedIconOverlay = new ItemizedIconOverlay<OverlayItem>(
+				this, overlayItemArray, null);
+		// Add the overlay to the MapView
+		mapView.getOverlays().add(itemizedIconOverlay);
 	}
 
 	// Adds an actionbar to the fragment
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.actionbar_fragment_map, menu);
+		addObstacleMenuItem = menu.findItem(R.id.addobstacle_item);
 		return true;
 
 	}
@@ -87,7 +98,7 @@ public class MapFragment extends Activity implements LocationListener {
 		// If we press the addObstacle in the actionbar, call the addObstacle
 		// function in MainActivity
 		case R.id.addobstacle_item:
-			// Call IT
+			addObstacle();
 			return true;
 		case R.id.visualsettings_item:
 			// Call IT
@@ -95,6 +106,14 @@ public class MapFragment extends Activity implements LocationListener {
 		default:
 			return super.onOptionsItemSelected(item);
 		}
+	}
+
+	private void addObstacle() {
+
+		if (addObstacleEnabled = true) {
+			addObstacleMenuItem.setIcon(R.drawable.icon_addobstacle_enabled);
+		}
+
 	}
 
 	@Override
