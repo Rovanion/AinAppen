c01fe8403b497ecc10e4a711c173ba6f8c42e0ad
elias.karnsund <elias.karnsund@gmail.com>
2014-04-29 14:26:58 +0200

    Hämtar batterinivå

--- src/gov/polisen/ainappen/DeviceStatusUpdater.java
+++ src/gov/polisen/ainappen/DeviceStatusUpdater.java
@@ -1,5 +1,49 @@
 package gov.polisen.ainappen;
 
-public class DeviceStatusUpdater {
+import java.util.TimerTask;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.util.Log;
+
+public class DeviceStatusUpdater extends TimerTask {
+
+	BroadcastReceiver mBatInfoReceiver;
+	Intent batteryStatus;
+	Context context;
+
+	public DeviceStatusUpdater(Context context) {
+
+		// // TODO get battery information and location
+		// BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
+		// @Override
+		// public void onReceive(Context context, Intent intent) {
+		// // TODO Auto-generated method stub
+		// int level = intent.getIntExtra("level", 0);
+		// }
+		// };
+		//
+		// this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(
+		// Intent.ACTION_BATTERY_CHANGED));
+
+		IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+		batteryStatus = context.registerReceiver(null, ifilter);
+		this.context = context;
+
+	}
+
+	@Override
+	public void run() {
+		int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+		int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+		float batteryPct = level / (float) scale;
+		// Toast.makeText(context, "Batteri" + batteryPct, Toast.LENGTH_SHORT)
+		// .show();
+		Log.d("TAG", "Batteri " + batteryPct);
+
+	}
 }
