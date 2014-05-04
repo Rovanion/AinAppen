bf0182c9036d9e17773cd29f05cda7ebe7b04bba
origin <elias.karnsund@gmail.com>
2014-05-05 01:31:40 +0200

    småfix

--- src/gov/polisen/ainappen/LoginAcitivity.java
+++ src/gov/polisen/ainappen/LoginAcitivity.java
@@ -8,7 +8,6 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -92,7 +91,6 @@ public class LoginAcitivity extends Activity {
 
 		public void makeGlobal() {
 			final GlobalData appData = ((GlobalData) getApplicationContext());
-			Log.d("TAG", appData.toString());
 			Random rnd = new Random();
 			int dId = rnd.nextInt(1000000);
 			appData.getUser().setUserName((userNameText.getText().toString()));
